87230561310c47fe817aee189775c5e018235980
duser <duser@duser-dekstop.com>
2016-09-23 12:37:19 +0300

    .

--- ssh/cipher.go
+++ ssh/cipher.go
@@ -168,7 +168,7 @@ func (s *streamPacketCipher) readPacket(seqNum uint32, r io.Reader) ([]byte, err
 	}
 
 	if length > maxPacket {
-		return nil, errors.New("ssh: invalid packet length, packet too large length:"+ length + " maxPacket:" + maxPacket)
+		return nil, errors.New("ssh2: invalid packet length, packet too large length:"+ length + " maxPacket:" + maxPacket)
 	}
 
 	// the maxPacket check above ensures that length-1+macSize
